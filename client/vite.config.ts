8c61f8d33335c979402e5069cd6e20ec6fb29b89
Pendela V.P.B Krishna Chaitanya <67474140+PhaniPendela@users.noreply.github.com>
2025-07-11 13:15:02 +0530

    Modified to call server and client at the same time

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,13 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5100/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [
     react(),
